c2ed924887bc1cb80ab4679db4e450a625f5e88c
tigi <onlytg@naver.com>
2018-11-22 17:55:58 +0900

    Add tests for each scheme

---
+++ 6_CustomLayoutCollectionViewExampleTests/__CustomLayoutCollectionViewExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  __CustomLayoutCollectionViewExampleTests.m
+//  6_CustomLayoutCollectionViewExampleTests
+//
+//  Created by tigi on 22/11/2018.
+//  Copyright © 2018 tigi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __CustomLayoutCollectionViewExampleTests : XCTestCase
+
+@end
+
+@implementation __CustomLayoutCollectionViewExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
